96e6b2d2f378bb7057a61b8d423818199956206a
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-01-17 00:50:23 -0500

    Add color picker

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 const GRID_SIZE = 16;
 let mouseDown = false;
+let color = "black";
+const canvas = document.getElementById("canvas")
+const colorPicker = document.getElementById("color-picker");
+
 // create a grid of 16x16 when page/window is loaded
 window.addEventListener("load", () => createCanvas(GRID_SIZE));
 
-
-const canvas = document.getElementById("canvas")
 canvas.addEventListener("mousedown", () => { mouseDown = true; } );
 canvas.addEventListener("mouseup", () => { mouseDown = false; } );
+
+colorPicker.addEventListener("input", setColor);
 
 function createCanvas(size) {
 
@@ -22,8 +26,12 @@ function createCanvas(size) {
 
 function sketchGrid(event) {
     if(event.type === "click") {
-        event.target.style.backgroundColor = "black";
+        event.target.style.backgroundColor = color;
     } else if (mouseDown) {
-        event.target.style.backgroundColor = "black";
+        event.target.style.backgroundColor = color;
     }
+}
+
+function setColor(event) {
+    color = event.target.value;
 }
