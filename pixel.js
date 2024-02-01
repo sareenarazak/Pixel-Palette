f819fa25a2e2399130f5386229e05e23f0572537
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-01 00:35:29 -0500

    Refactored button click event handlers

--- pixel.js
+++ pixel.js
@@ -1,92 +1,104 @@
-// DOM Elements
-const canvas = document.getElementById("canvas");
-const colorPicker = document.getElementById("color-picker");
-const colorChaosBtn = document.getElementById("color-chaos");
-const pixelSizeInput = document.getElementById("pixel-size");
-const clearButton = document.getElementById("clear");
-const eraseButton = document.getElementById("eraser");
-const pixelSizeLabel = document.querySelector(".pixel-size");
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("canvas");
+    const normalModeBtn = document.getElementById("color-normal");
+    const colorPicker = document.getElementById("color-picker");
+    const chaosModeBtn = document.getElementById("color-chaos");
+    const pixelSizeInput = document.getElementById("pixel-size");
+    const clearButton = document.getElementById("clear");
+    const eraseButton = document.getElementById("eraser");
+    const pixelSizeLabel = document.getElementById("pixel-size");
 
-// Variables
-let pixelSize = Number.parseInt(pixelSizeInput.value, 10);
-let mouseDown = false;
-let currentColor = "#000000"; // Default color black
-let mode = "normal";
+    let pixelSize = Number.parseInt(pixelSizeInput.value, 10);
+    let mouseDown = false;
+    let currentColor = "#000000"; // Default color black
+    let mode = "normal";
 
-
-// Event Listeners
-window.addEventListener("load", () => createPixelArtCanvas(pixelSize));
-
-canvas.addEventListener("mouseup", () => { mouseDown = false; } );
-canvas.addEventListener("mousedown", () => { mouseDown = true; } );
-
-colorPicker.addEventListener("input", setColor);
-colorChaosBtn.addEventListener("click", () => changeMode("chaos"));
-pixelSizeInput.addEventListener("input", changePixelSize);
-
-eraseButton.addEventListener("click", () => changeMode("erase"));
-clearButton.addEventListener("click", clearCanvas);
-
-
-function createPixelArtCanvas(pixelSize) {
-    let count = Math.floor(canvas.clientHeight/ pixelSize);
-
-    for(let index = 0; index <  count * count ; index++) {
-        const pixelDiv = document.createElement("div");
-        pixelDiv.style.width = `${pixelSize}px`;
-        pixelDiv.style.height = `${pixelSize}px`;
-        pixelDiv.classList.add("pixel-div");
-        canvas.appendChild(pixelDiv);
-
-        pixelDiv.addEventListener("click",  drawPixel);
-        pixelDiv.addEventListener("mouseenter",  drawPixel);
-    }
-}
-
-function drawPixel(event) {
-    if (mode === "chaos") {
-        currentColor = "#" + (Math.random() * 0xFFFFFF << 0).toString(16);
-    } else if (mode ==="erase") {
-        currentColor = "white";
-    }
-
-    if(event.type === "click" || (event.type === "mouseenter" &&  mouseDown)) {
-        event.target.style.backgroundColor = currentColor;
-    }
-}
-
-function setColor(event) {
-    mode = "normal";
-    currentColor = event.target.value;
-}
-
-function clearCanvas() {
-    const pixelDivs = document.querySelectorAll(".pixel-div");
-    pixelDivs.forEach(pixel => pixel.style.backgroundColor = "white");
-   // changeMode("normal"); --> Rethink this
-}
-
-function changePixelSize(event) {
-    deletePixels();
-    pixelSize = event.target.value;
-    pixelSizeLabel.innerText = `${pixelSize} x ${pixelSize}`;
     createPixelArtCanvas(pixelSize);
-}
 
-function deletePixels() {
-    canvas.innerHTML = "";
-    //pixelDivs.forEach(pixel => pixel.re)
-}
-function changeMode(newNode) {
-    mode = newNode;
-}
-function setPixelSize(event) {
-    console.log("Size before " + pixelSize);
-    pixelSize = event.target.value;
-    console.log("Size after " + pixelSize);
-}
+    canvas.addEventListener("mouseup", () => { mouseDown = false; });
+    canvas.addEventListener("mousedown", () => { mouseDown = true; });
 
-// TODO : 1) highlight the button for chaos mode +
-//  make the color picker mode only if the chaos mode is off and the other mode is on
-//  2 ) have a color picker that shoes the color picked
-//  3) make the label appear on the side of the color picker
+    normalModeBtn.addEventListener("click", handleNormalModeButtonClick);
+    colorPicker.addEventListener("input", handleColorPickerInputChange);
+
+    chaosModeBtn.addEventListener("click", handleChaosModeButtonClick);
+
+    eraseButton.addEventListener("click", handleEraseButtonClick);
+    clearButton.addEventListener("click", clearCanvas);
+
+    pixelSizeInput.addEventListener("input", handlePixelSizeInputChange);
+
+    function createPixelArtCanvas(pixelSize) {
+        let count = Math.floor(canvas.clientHeight / pixelSize);
+        for (let index = 0; index < count * count; index++) {
+            const pixelDiv = document.createElement("div");
+
+            pixelDiv.style.width = `${pixelSize}px`;
+            pixelDiv.style.height = `${pixelSize}px`;
+            pixelDiv.classList.add("pixel-div");
+
+            canvas.appendChild(pixelDiv);
+
+            pixelDiv.addEventListener("click", drawPixel);
+            pixelDiv.addEventListener("mouseenter", drawPixel);
+        }
+    }
+
+    function drawPixel(event) {
+        if (mode === "chaos") {
+            setColor("#" + (Math.random() * 0xFFFFFF << 0).toString(16));
+        } else if (mode === "erase") {
+            setColor("white");
+        }
+        if (event.type === "click" || (event.type === "mouseenter" && mouseDown)) {
+            event.target.style.backgroundColor = currentColor;
+        }
+    }
+
+    // TODO: refactor the 4 methods for mode changes / button clicks 
+    function handleNormalModeButtonClick() {
+        setMode("normal");
+        setColor(colorPicker.value);
+    }
+
+    function handleColorPickerInputChange(event) {
+        setMode("normal");
+        setColor(event.target.value);
+    }
+
+    function handleChaosModeButtonClick() {
+        setMode("chaos")
+    }
+
+    function handleEraseButtonClick() {
+        setMode("erase");
+    }
+
+    function handlePixelSizeInputChange(event) {
+        deletePixels();
+        setPixelSize(event.target.value);
+        createPixelArtCanvas(pixelSize);
+    };
+
+    function clearCanvas() {
+        const pixelDivs = document.querySelectorAll(".pixel-div");
+        pixelDivs.forEach(pixel => pixel.style.backgroundColor = "white");
+    }
+
+    function setColor(color) {
+        currentColor = color;
+    }
+
+    function setMode(newMode) {
+        mode = newMode;
+    }
+
+    function setPixelSize(size) {
+        pixelSize = size;
+        pixelSizeLabel.innerText = `${pixelSize} x ${pixelSize}`;
+    }
+
+    function deletePixels() {
+        canvas.innerHTML = "";
+    }
+});
